8e87d5dc873f55b7bb825598a766a29db202795f
robser37 <rob@dunklerwald.xyz>
2017-02-20 22:29:25 +0100

    Reading structure and print it as tree. No xml-tags yet.

--- edifact_docu_parser.py
+++ edifact_docu_parser.py
@@ -1,67 +1,46 @@
 import urllib
 
-page = open('edifact_structure.txt')
-
+plain_text = open('edifact_structure.txt')
+# to track if group is currently open (True) or closed (False)
 tracker = {0: False, 1: False, 2: False, 3: False, 4: False, 5: False, 6: False}
+# to stack to track group names
+groups = []
 
-def isOpen(indent):
-    return tracker.get(indent)
-
-
-# print "result: " + str(isOpen(0))
-# print "result: " + str(isOpen(1))
-
-
-
-for line in page:
+for line in plain_text:
     if 'DETAIL' not in line and len(line.split()) > 0:
         struc_str = (line[63:70])[::-1]
+        group = ''
+        segment = ''
+        ind_lvl = '  '
+        if 'Segment group' in line:
+            group_str = line[17:33].rsplit()
+            group = group_str[1] + "_" + group_str[2]
+        else:
+            segment = line[7:10]
         if struc_str != "\n" and len(struc_str) > 0:
-            new = (struc_str.replace("-", "")).rstrip()
-            struc_list = list(new)
-            print new
-            #print "curr_sym: " + new
-            # print "A----------------------"
-            while len(struc_list) != 0:
-                indent = len(struc_list) - 1
-                sym = struc_list.pop()
-                # print "line: " + new
-                # print "indent: " + str(indent)
-                # print "sym: " + sym
-                if sym == '+':
-                    if isOpen(indent):
-                        print "close"
-                        tracker[indent] = False
+            struc_str = (struc_str.replace("-", "")).rstrip()
+
+            max_indent = len(struc_str) - 1
+            while len(struc_str) != 0:
+                indent = len(struc_str) - 1
+                chr = struc_str[-1]
+                struc_str = struc_str[:-1]
+                if chr == '|':
+                    if len(segment.split()) == 0 or indent != max_indent:
+                        break
+                    print ind_lvl * (indent + 1) + segment
+                    break
+                elif chr == '+' and tracker.get(indent) == True:
+                    if indent == max_indent:
+                        print ind_lvl * (indent + 1) + segment
+                        print ind_lvl * indent + groups.pop()
                     else:
-                        print "open"
-                        tracker[indent] = True
-                # stop = raw_input()
-                # print tracker
-            # print "Z----------------------"
-
-
-
-        # if len(struc_str) > 0:
-        #     indent = 0
-        #     last_sym = '0'
-        #
-        #     for sym in struc_str:
-        #         if sym == '+' and indent == 0 and open == False:
-        #             print 'group-open'
-        #             open = True
-        #
-        #         elif last_sym == '|' and sym == '+' and open == True
-        #             print 'sub-group-open'
-        #
-        #
-        #
-        #
-        #
-        #
-        #
-        #         elif sym == '+' and indent == 0 and open == True:
-        #             print 'group-close'
-        #             open = False
-        #         # elif sym == '+' and group_open == True:
-        #         #     print 'group-close'
-        #         last_sym = sym
+                        print ind_lvl * indent + groups.pop()
+                    tracker[indent] = False
+                elif chr == '+' and tracker.get(indent) == False:
+                    print ind_lvl * indent + group
+                    groups.append(group)
+                    tracker[indent] = True
+        else:
+            if len(segment) > 0:
+                print segment
